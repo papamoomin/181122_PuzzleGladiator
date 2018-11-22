d82f5ab1b025116fa8d2b3a0a9c6517acbc0aef7
moominpapa <c_p_w_et@naver.com>
2018-11-22 16:47:45 +0900

    첫 업로드

---
+++ Assets/Scripts/Connect.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class Connect : MonoBehaviourPunCallbacks
+{
+    string gameVersion = "1";
+    private void Awake()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+
+    public void ConnectClient()
+    {
+        if (!PhotonNetwork.IsConnected)
+        {
+            PhotonNetwork.GameVersion = gameVersion;
+            PhotonNetwork.ConnectUsingSettings();
+            print("connect start");
+        }
+    }
+
+
+
+    public override void OnConnectedToMaster()
+    {
+        print("connect finish");
+        PhotonNetwork.JoinRandomRoom(null, 2);
+        print("make or join room start");
+    }
+
+    public override void OnCreatedRoom()
+    {
+        print("Create room finish");
+    }
+
+    public override void OnJoinedRoom()
+    {
+        print("make or join room finish");
+        print("now Player : " + PhotonNetwork.CurrentRoom.PlayerCount);
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        print("make or join room failed. Create Room Start");
+        PhotonNetwork.CreateRoom(null, new Photon.Realtime.RoomOptions { MaxPlayers = 2 });
+    }
+}
